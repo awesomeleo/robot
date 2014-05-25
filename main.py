b8c43359969adb08bfdcf8586ea2667cc1e6dafe
Anton Eriksson <antoneri@gmail.com>
2014-05-25 21:08:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import cv2
+
+import vision
+
+
+STATIC = True
+
+if STATIC:
+    img = cv2.imread('grids.jpg')
+    img = cv2.resize(img, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+
+    __, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    contours, __ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    vision.main_loop(img, gray, contours)
+
+    cv2.imshow('Main window', img)
+    cv2.waitKey(0)
+
+else:
+    cap = cv2.VideoCapture(0)
+
+    while 1:
+        __, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.medianBlur(gray, 5)
+
+        __, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+        contours, __ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        vision.main_loop(img, gray, contours)
+
+        cv2.imshow('Main window', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+
+cv2.destroyAllWindows()
---
+++ vision/vision.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+
+BLUE = (255, 50, 50)
+GREEN = (50, 255, 50)
+RED = (50, 50, 255)
+
+MARKER_SIZE = 5 * 5  # num squares
+MARKER_ROI = 3 * 3
+
+SQUARE_SIZE = 60  # pixels
+ROI_WIDTH = SQUARE_SIZE * 5
+ROI_HEIGHT = SQUARE_SIZE * 5
+
+# clock-wise
+TRANSFORM_MAT = np.float32([[0, 0], [ROI_WIDTH, 0], [ROI_WIDTH, ROI_HEIGHT], [0, ROI_HEIGHT]])
+
+
+def small_area(region):
+    return cv2.contourArea(region) < 1e2
+
+
+def not_quadrilateral(points):
+    return len(points) != 4
+
+
+def no_black_border(region):
+    left = cv2.mean(region[0:60])
+    right = cv2.mean(region[240:300])
+    top = cv2.mean(region[:, 0:60])
+    bottom = cv2.mean(region[:, 240:300])
+    mean = np.mean(left + right + top + bottom)
+    return mean > 10
+
+
+def main_loop(img, gray, contours):
+    for i, contour in enumerate(contours):
+
+        if small_area(contour):
+            continue
+
+        epsilon = 0.05 * cv2.arcLength(contour, closed=True)
+        polygon = cv2.approxPolyDP(contour, epsilon, closed=True)
+
+        if not_quadrilateral(polygon):
+            continue
+
+        # TODO: rotate in the same direction first!
+
+        # perspective transform the polygon
+        polygon_f = np.float32(polygon)
+        M = cv2.getPerspectiveTransform(polygon_f, TRANSFORM_MAT)
+        square = cv2.warpPerspective(gray, M, (ROI_WIDTH, ROI_HEIGHT))
+        __, square_bin = cv2.threshold(square, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        if no_black_border(square_bin):
+            continue
+
+        bitmap_candidate = np.zeros(shape=(3, 3))
+
+        for row, j in zip(range(90, 240, 60), range(3)):
+            for col, k in zip(range(90, 240, 60), range(3)):
+                if square_bin[row, col] == 0:
+                    bitmap_candidate[j, k] = 0
+                elif square_bin[row, col] == 255:
+                    bitmap_candidate[j, k] = 1
+
+        # cv2.imshow('data {num}'.format(num=i), roi)
+
+        marker_id = "id={num}".format(num=i)
+        id_pos = tuple(map(int, polygon.min(axis=0)[0]))
+
+        cv2.drawContours(img, [polygon], -1, GREEN, 2)
+        cv2.putText(img, marker_id, id_pos, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.6, color=RED)
